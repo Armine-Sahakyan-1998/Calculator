7c554c6f44a1e9eca2df1c2de8b2edfdfebd9cd0
Armine-Sahakyan-1998 <arm1998.sahakyan@mail.ru>
2026-06-13 10:35:01 +0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,47 @@
+def number():
+    while True:
+        try:
+            number = float(input('number: '))
+            return number
+        except ValueError:
+            print('write only number')
+
+def choice():
+    ch = ('+', '-', '/', '*')
+    c = input("'+', '-', '/', '*'")
+    try:
+        if c not in ch:
+            raise Exception
+    except:
+        print("only",ch)
+    return c
+
+def result():
+    x = number()
+    c = choice()
+    y = number()
+    if c == '+':
+        res = x + y
+        return f"{x} + {y} = {res}"
+    elif c == '-':
+        res = x - y
+        return f"{x} - {y} = {res}"
+    elif c == '*':
+        res = x * y
+        return f"{x} * {y} = {res}"
+    elif c == '/':
+        while True:
+            try:
+                res = x / y
+                return f"{x} / {y} = {res}"
+            except ZeroDivisionError:
+                print("Zeroyi bajanel chenq karox: ")
+                y = number()
+                
+                
+
+
+
+
+
+        
